5d04b2fcd3299d6a90535fc46334d6938e8b3c1b
AdnanTurjoy <aturjoy4@gmail.com>
2022-09-28 12:35:46 +0600

    Todo added Modal showwww

--- src/todo/Home.js
+++ src/todo/Home.js
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
-import NewTodo from './newTodo';
-import Todos from './Todos';
+import React, { useState } from "react";
+import NewTodo from "./newTodo";
+import Todos from "./Todos";
 function HomeTodos() {
-    const [todo,setTodo]=useState(["adnan1","Turjoy2"])
-    const handleAddTodo=(add)=>{
-        setTodo([...todo,add])
-        
-    }
-    return ( 
-        <div>
-            <NewTodo addTodo={handleAddTodo}/>
-            <Todos todos={todo}/>
-        </div>
-        
-     );
+  const [todo, setTodo] = useState(["adnan1", "Turjoy2"]);
+  const [modal, setModal] = useState(false);
+  const handleAddTodo = (add) => {
+    setTodo([...todo, add]);
+    setModal(true);
+    setTimeout(() => {
+      setModal(false);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      {modal && (
+        <p
+          style={{
+            backgroundColor: "#52f794",
+            width: "500px",
+            textAlign: "center",
+            margin: "auto",
+          }}
+        >
+          Todo is added
+        </p>
+      )}
+      <NewTodo addTodo={handleAddTodo} />
+
+      <Todos todos={todo} />
+    </div>
+  );
 }
 
 export default HomeTodos;
--- src/todo/newTodo.js
+++ src/todo/newTodo.js
@@ -1,27 +1,29 @@
-import React, { useState } from 'react';
-function NewTodo({addTodo}) {
-  const [newTodo,setNewTodo] = useState('');
-  const handleNewTodo =(e)=>{
+import React, { useState } from "react";
+function NewTodo({ addTodo }) {
+  const [newTodo, setNewTodo] = useState("");
+  const handleNewTodo = (e) => {
     setNewTodo(e.target.value);
-    
-  }
-  const handleSubmit =(e)=>{
+  };
+  const handleSubmit = (e) => {
     e.preventDefault();
-    addTodo(newTodo)
-    setNewTodo('')
-  }
-  console.log("newTodo:",newTodo);
-    return ( 
-        <form action="" onSubmit={handleSubmit}>
-            <div className='form-group'>
-              <label>Add Your Todo: </label>
-              <input type="text" onChange={handleNewTodo} value={newTodo} name="newNode" />
-              <button type="submit"> Submit</button>
-            </div>
-            
-
-        </form>
-     );
+    addTodo(newTodo); // child to parent system
+    setNewTodo("");
+  };
+  console.log("newTodo:", newTodo);
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Add Your Todo: </label>
+        <input
+          type="text"
+          onChange={handleNewTodo}
+          value={newTodo}
+          name="newNode"
+        />
+        <button type="submit"> Submit</button>
+      </div>
+    </form>
+  );
 }
 
 export default NewTodo;
